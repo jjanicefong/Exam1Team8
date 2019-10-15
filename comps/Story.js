0f60ac83dba0160efae44031b2ecdfcb84bf3fac
josephbondoc <josephsantosbondoc@gmail.com>
2019-10-15 10:03:26 -0700

    Who knows WTF?!

---
+++ comps/Story.js
@@ -0,0 +1,67 @@
+import React, {useState} from 'react';
+import 
+{View,
+StyleSheet,
+Switch,
+Text} from 'react-native'
+import styles from '../styles/Storystyles';
+export default function Story(){
+
+    // NIGHT MODE BUTTON START
+const [nightMode, setNightMode] = useState(false);
+
+    var statemsg = null;
+    var bgcolor = null;
+    if (nightMode === false) {
+        statemsg = (
+            <Text>
+                Minions ipsum la bodaaa hana dul sae daa belloo! Daa poulet tikka masala tatata bala tu bappleees. Gelatooo hana dul sae jiji pepete para tú butt gelatooo potatoooo me want bananaaa! Po kass underweaaar wiiiii chasy para tú hahaha bananaaaa pepete wiiiii. Aaaaaah baboiii po kass poopayee tatata bala tu underweaaar jeje pepete ti aamoo! Bee do bee do bee do ti aamoo! Hahaha belloo! Underweaaar tatata bala tu ti aamoo! Daa chasy poulet tikka masala.
+            </Text>
+        )
+            bgcolor = styles.app
+    }
+        else {
+            statemsg = (
+                <Text style={{color:'white'}}>
+                    Minions ipsum la bodaaa hana dul sae daa belloo! Daa poulet tikka masala tatata bala tu bappleees. Gelatooo hana dul sae jiji pepete para tú butt gelatooo potatoooo me want bananaaa! Po kass underweaaar wiiiii chasy para tú hahaha bananaaaa pepete wiiiii. Aaaaaah baboiii po kass poopayee tatata bala tu underweaaar jeje pepete ti aamoo! Bee do bee do bee do ti aamoo! Hahaha belloo! Underweaaar tatata bala tu ti aamoo! Daa chasy poulet tikka masala.
+                </Text>
+            )
+            bgcolor = styles.night
+        }
+        // NIGHT MODE BUTTON END
+
+
+        // SCROLL WRAP BUTTON START
+const [scrollText, setScrollText] = useState(false);
+        var textscroll = null;
+        
+        // if button true, text scroll
+        if (scrollText === true) {
+            <scrollText>
+                
+            </scrollText>
+
+        }
+
+        
+
+        // UI STATE
+        return (
+            <View style={bgcolor}>
+                <Switch 
+                onValueChange = {(value)=>setNightMode(value)}
+                value = {nightMode}
+                />
+                <Switch
+                onValueChange = {(value)=>setScrollText(value)}
+                value = {scrollText}
+                />
+                <Text>
+                    Night Mode
+                </Text>
+                {statemsg}
+
+            </View>
+    )
+}
+ 
---
+++ styles/Storystyles.js
@@ -0,0 +1,24 @@
+import {StyleSheet} from 'react-native';
+ 
+var styles = StyleSheet.create({
+    app: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+        height: "100%",
+        padding:'3%'
+
+    },
+    night: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+        height:"100%",
+        backgroundColor: '#262626',
+        padding:'3%'
+    }
+});
+ 
+export default styles;
