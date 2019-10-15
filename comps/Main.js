0a012eb659d3bde83895e3c0e6d17767c0df2dc3
josephbondoc <josephsantosbondoc@gmail.com>
2019-10-15 11:51:21 -0700

    committed story

---
+++ comps/Main.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {View} from 'react-native';
+import styles from '../styles/Mainstyles';
+import Story from './Story';
+ 
+function Main(){
+  // UI
+  return (
+    <View style={styles.main}>
+    <Story />
+    </View>
+  )
+}
+ 
+export default Main;
